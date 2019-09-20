737ec4263cb476f1e2bfda6cf3de201ca7f11210
Chadzelda <54717796+Chadzelda@users.noreply.github.com>
2019-09-20 17:27:30 -0400

    Add files via upload

---
+++ Upload Pictures to Server php/phpTest.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+   <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+	<head> 
+	<style> 
+	
+	#Content{ 
+	text-align: center; 
+	margin-top: 5%;
+	} 
+	</style> 
+	</head> 
+	
+	 
+	<body> 
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+	<br> 
+	<br> 
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <br> 
+	<br> 
+	<input type="submit" value="Upload Image" name="submit">
+	<br> 
+	<br> 
+	</form>
+	<input 
+	<ul> 
+	
+	<!--  Php
+	#$MyArray = ["A", "B", "C"];  
+	
+		
+	#for(int $i = 0; $i < $MyArray.length; $i++) { 
+	
+	#foreach( $MyArray as $i) { 
+	#echo "<li>" . $i . "</li>"  ; 
+
+	#}
+	--> 
+	
+	</ul>
+	</body> 
+	</html> 
